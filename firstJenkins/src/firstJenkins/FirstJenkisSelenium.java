1fe289fade9db91231342df76e434eebd479f018
REALTECH\anna.zhuravleva <anna.zhuravleva@techedgegroup.com>
2018-03-01 17:03:24 +0100

    Initial commit

---
+++ firstJenkins/src/firstJenkins/FirstJenkisSelenium.java
@@ -0,0 +1,12 @@
+package firstJenkins;
+
+import org.testng.annotations.Test;
+
+public class FirstJenkisSelenium {
+
+	@Test
+	public void test1() {
+		
+		System.out.print("Mi primer test con Jenkins en selenium TestNG");
+	}
+}
